520104867be95b083a54fa8b77ad5ae231a74c67
sahinbingol85 <sahinbingol@gmail.com>
2026-09-21 10:48:48 +0300

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  // Sitemap'in linkleri oluşturabilmesi için zorunlu alan:
+  site: 'https://www.bekegitimakademi.com',
+  
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  
+  integrations: [sitemap()]
 });
